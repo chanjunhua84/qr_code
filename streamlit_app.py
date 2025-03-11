3d9d9792598c7cc4be5c2061731f6aed6db269a4
chanjunhua84 <chan_jun_hua@mlaw.gov.sg>
2025-03-11 20:22:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,8 @@ def get_letter_content(letter_id):
         content = letter["content"]
         
         # Store in session state for later use
-        st.session_letterTitle['current_title'] = title
+        st.session_state['current_title'] = title
+        st.session_state['current_content'] = content st.session_letterTitle['current_title'] = title
         st.session_state.extracted_text['current_content'] = content
         
         # Display content
